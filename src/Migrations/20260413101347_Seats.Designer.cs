113cb36a28967d6095caaa46fa434d03c3af7971
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-04-13 13:27:49 +0300

    Implemented swagger, docs

--- src/Migrations/20260413101347_Seats.Designer.cs
+++ src/Migrations/20260413101347_Seats.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using EventTickets.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EventTickets.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260413101347_Seats")]
+    partial class Seats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
